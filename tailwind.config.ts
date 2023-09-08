a7360f01ee1a167037bd2c0b1a564f42838f945c
Brandon Ting <brandon1221t@gmail.com>
2023-09-08 10:39:34 +0800

    modify settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,12 @@
-import { skeleton } from '@skeletonlabs/tw-plugin';
-import { join } from 'path'
 import type { Config } from 'tailwindcss';
 
 export default {
 	darkMode: 'class',
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
-		// 3. Append the path to the Skeleton package
-		join(require.resolve(
-			'@skeletonlabs/skeleton'),
-			'../**/*.{html,js,svelte,ts}'
-		)
 	],
 	theme: {
 		extend: {},
 	},
-	plugins: [skeleton({
-		themes: { preset: ["skeleton"] }
-	})],
 } satisfies Config
 
